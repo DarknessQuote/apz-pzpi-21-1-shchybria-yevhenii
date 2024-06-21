0ddb01aad8a2af88abccaf0815db2b57cd61981e
Yevhenii Shchybria <yevhenii.shchybria@nure.ua>
2024-06-21 03:51:04 +0300

    WEB: Implemented company service

--- Task4/apz-pzpi-21-1-shchybria-yevhenii-task4/devquest-web/src/services/companyService.js
+++ Task4/apz-pzpi-21-1-shchybria-yevhenii-task4/devquest-web/src/services/companyService.js
@@ -24,3 +24,128 @@ export const getCompanies = async () => {
 		throw err;
 	}
 };
+
+export const getCompany = async (companyID, token) => {
+	try {
+		const headers = new Headers();
+		headers.append("Content-Type", "application/json");
+		headers.append("Authorization", `Bearer ${token}`);
+
+		const reqOptions = {
+			method: "GET",
+			headers: headers,
+			credentials: "include",
+		};
+
+		const responseJSON = await fetch(
+			`http://127.0.0.1:8080/companies/${companyID}`,
+			reqOptions
+		);
+		const response = await responseJSON.json();
+
+		if (response.error) {
+			throw new Error(response.message);
+		}
+
+		return response;
+	} catch (err) {
+		throw err;
+	}
+};
+
+export const addCompany = async (addCompanyData, token) => {
+	try {
+		const headers = new Headers();
+		headers.append("Content-Type", "application/json");
+		headers.append("Authorization", `Bearer ${token}`);
+
+		const reqBody = {
+			name: addCompanyData.name,
+			owner: addCompanyData.owner,
+			email: addCompanyData.email,
+		};
+
+		const reqOptions = {
+			method: "POST",
+			headers: headers,
+			credentials: "include",
+			body: JSON.stringify(reqBody),
+		};
+
+		const responseJSON = await fetch(
+			"http://127.0.0.1:8080/companies",
+			reqOptions
+		);
+		const response = await responseJSON.json();
+
+		if (response.error) {
+			throw new Error(response.message);
+		}
+
+		return response;
+	} catch (err) {
+		throw err;
+	}
+};
+
+export const updateCompany = async (companyID, updateCompanyData, token) => {
+	try {
+		const headers = new Headers();
+		headers.append("Content-Type", "application/json");
+		headers.append("Authorization", `Bearer ${token}`);
+
+		const reqBody = {
+			name: updateCompanyData.name,
+			owner: updateCompanyData.owner,
+			email: updateCompanyData.email,
+		};
+
+		const reqOptions = {
+			method: "PUT",
+			headers: headers,
+			credentials: "include",
+			body: JSON.stringify(reqBody),
+		};
+
+		const responseJSON = await fetch(
+			`http://127.0.0.1:8080/companies/${companyID}`,
+			reqOptions
+		);
+		const response = await responseJSON.json();
+
+		if (response.error) {
+			throw new Error(response.message);
+		}
+
+		return response;
+	} catch (err) {
+		throw err;
+	}
+};
+
+export const deleteCompany = async (companyID, token) => {
+	const headers = new Headers();
+	headers.append("Authorization", `Bearer ${token}`);
+
+	try {
+		const reqOptions = {
+			method: "DELETE",
+			credentials: "include",
+			headers: headers,
+		};
+
+		const responseJSON = await fetch(
+			`http://127.0.0.1:8080/companies/${companyID}`,
+			reqOptions
+		);
+		const response = await responseJSON.json();
+
+		if (response.error) {
+			throw new Error(response.message);
+		}
+
+		return response;
+	} catch (err) {
+		throw err;
+	}
+};
