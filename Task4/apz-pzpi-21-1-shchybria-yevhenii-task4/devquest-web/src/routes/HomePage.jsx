4a105253f5c2debe039e540d12bc8e023747d82d
Yevhenii Shchybria <yevhenii.shchybria@nure.ua>
2024-06-24 15:54:24 +0300

    WEB: Added achievement showcase to developer home page

--- Task4/apz-pzpi-21-1-shchybria-yevhenii-task4/devquest-web/src/routes/HomePage.jsx
+++ Task4/apz-pzpi-21-1-shchybria-yevhenii-task4/devquest-web/src/routes/HomePage.jsx
@@ -1,11 +1,12 @@
 import { useTranslation } from "react-i18next";
 import { useAuthContext } from "../context/AuthContext";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Divider, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getUser } from "../services/userService";
 import UserInfo from "../components/UserInfo";
 import { useNavigate } from "react-router-dom";
 import { dataBackup } from "../services/adminService";
+import DeveloperAchievements from "../components/DeveloperAchievements";
 
 const HomePage = () => {
 	const [auth] = useAuthContext();
@@ -32,18 +33,29 @@ const HomePage = () => {
 		);
 	} else if (user !== null) {
 		return (
-			<Box className="flex gap-5 items-center">
-				<Box className="w-3/5">
-					<UserInfo userInfo={user} />
+			<Box>
+				<Box className="flex gap-5 items-center">
+					<Box className="w-3/5">
+						<UserInfo userInfo={user} />
+					</Box>
+					<Box className="w-2/5 flex flex-col gap-3 justify-stretch items-stretch">
+						<Typography
+							variant="h5"
+							align="center"
+							className="mb-3">
+							{t("availableActions")}
+						</Typography>
+						{auth.role === "Admin" && <AdminActions />}
+						{auth.role === "Manager" && <ManagerActions />}
+						{auth.role === "Developer" && <DeveloperActions />}
+					</Box>
 				</Box>
-				<Box className="w-2/5 flex flex-col gap-3 justify-stretch items-stretch">
-					<Typography variant="h5" align="center" className="mb-3">
-						{t("availableActions")}
-					</Typography>
-					{auth.role === "Admin" && <AdminActions />}
-					{auth.role === "Manager" && <ManagerActions />}
-					{auth.role === "Developer" && <DeveloperActions />}
-				</Box>
+				{auth.role === "Developer" && (
+					<>
+						<Divider className="my-8" />
+						<DeveloperAchievements />
+					</>
+				)}
 			</Box>
 		);
 	}
